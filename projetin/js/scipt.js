25937dc3efecb9fcb36f1efb648dfbba872a9380
Filipe Santos <143547860+Filipe-S-Silva@users.noreply.github.com>
2024-05-30 17:30:58 -0300

    Add files via upload

--- projetin/js/scipt.js
+++ projetin/js/scipt.js
@@ -1,35 +1,56 @@
-document.querySelector('#olho').addEventListener('click', ()=>{
-    
+document.querySelector('#olho').addEventListener('click', () => {
 
-    if ( document.getElementById('senha').type == 'password') {
-    
-    
+    if (document.getElementById('senha').type == 'password') {
+
+
         document.getElementById('olho').src = 'img/olhoAberto.png'
-        document.getElementById('senha').type = 'text' 
-        
-        
+        document.getElementById('senha').type = 'text'
+
+
     } else {
-    
-        
+
+
         document.getElementById('olho').src = 'img/olhoFechado.png'
-        document.getElementById('senha').type = 'password' 
+        document.getElementById('senha').type = 'password'
     }
 })
 
-document.getElementById('btn').addEventListener('click', ()=>{
-
+document.getElementById('btn').addEventListener('click', () => {
     validacaoTec = (document.getElementById('login').value == 'TECNICO' && document.getElementById('senha').value == '123TEC');
-    validacaUSER = (document.getElementById('login').value == 'USER' && document.getElementById('senha').value == '123USER');
+    validacaoUSER = (document.getElementById('login').value == 'USER' && document.getElementById('senha').value == '123USER');
 
-    if (validacaoTec || validacaUSER) {
+    if (validacaoTec || validacaoUSER) {
         if (validacaoTec == true) {
-            window.location.href = 'html/tecnico.html'
+            window.location.href = 'tec/tecnico.html'
         } else {
-            window.location.href = 'html/user.html'            
+            window.location.href = 'user/user.html'
         }
     } else {
         document.getElementById('login').value = ''
         document.getElementById('senha').value = ''
-        alert('LOGIN OU SENHA INVÁLIDOS')
+        document.getElementById('alert').textContent = 'LOGIN OU SENHA INVÁLIDOS, TENTE NOVAMENTE!!!'
     }
 })
+
+document.addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+        login()
+    }
+})
+
+function login() {
+    validacaoTec = (document.getElementById('login').value == 'TECNICO' && document.getElementById('senha').value == '123TEC');
+    validacaoUSER = (document.getElementById('login').value == 'USER' && document.getElementById('senha').value == '123USER');
+
+    if (validacaoTec || validacaoUSER) {
+        if (validacaoTec == true) {
+            window.location.href = 'tec/tecnico.html'
+        } else {
+            window.location.href = 'user/user.html'
+        }
+    } else {
+        document.getElementById('login').value = ''
+        document.getElementById('senha').value = ''
+        document.getElementById('alert').textContent = 'LOGIN OU SENHA INVÁLIDOS, TENTE NOVAMENTE!!!'
+    }
+}
